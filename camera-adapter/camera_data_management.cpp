81a9bc331e55652df7d17370eb114590049f4196
Pedro Almeida <pedro22@ua.pt>
2024-02-26 16:59:08 +0000

    add camera-adapter & prepare services to use a single config.ini file

---
+++ camera-adapter/camera_data_management.cpp
@@ -0,0 +1,175 @@
+#include "camera_data_management.h"
+
+std::list<cameraMqttObject> parse_json(const std::string& mqtt_camera_object) {
+    // Parse the JSON string
+    rapidjson::Document document;
+    document.Parse(mqtt_camera_object.c_str());
+
+    // Check if parsing succeeded
+    if (document.HasParseError() or !document.IsObject()) {
+        spdlog::warn("Failed to parse JSON");
+        return {};  // Return an empty std::list when parsing fails
+    }
+
+    std::list<cameraMqttObject> objs;
+    double timestamp = document["timestamp"].GetDouble();
+
+    const rapidjson::Value& mqttListOfObjects = document["listOfObjects"];
+
+    uint list_size = mqttListOfObjects.Size();
+    for (rapidjson::SizeType i = 0; i < list_size; i++) {
+        const rapidjson::Value& mqtt_obj = mqttListOfObjects[i];
+
+        cameraMqttObject obj{};
+
+        obj.objectID = mqtt_obj["objectID"].GetInt();
+        obj.classification = mqtt_obj["classification"].GetInt();
+        obj.confidence = mqtt_obj["confidence"].GetInt();
+        obj.latitude = mqtt_obj["latitude"].GetDouble();
+        obj.longitude = mqtt_obj["longitude"].GetDouble();
+
+        if (!mqtt_obj["heading"].IsNull())
+            obj.heading = mqtt_obj["heading"].GetDouble();
+        else
+            obj.heading = 0.0;
+
+        // Check if speed is null
+        if (!mqtt_obj["speed"].IsNull())
+            obj.speed = mqtt_obj["speed"].GetDouble();
+        else
+            obj.speed = 0.0;
+
+        obj.timestamp = timestamp;
+
+        objs.push_back(obj);
+//        spdlog::info("camera mqtt obj: \"{}\"\n", obj.objectID);
+    }
+
+    return objs;
+}
+
+bool calc_is_new_info(std::mutex* lock, std::map<int, cameraMqttObject> *dict, cameraMqttObject camera_object) {
+    std::lock_guard guard(*lock);
+
+    // if not in "last_sent" is newInfo
+    if (dict->find(camera_object.objectID) == dict->end()) {
+        return true;
+    }
+
+    int obj_id  = camera_object.objectID;
+
+    double delta_timestamp = camera_object.timestamp - dict->at(obj_id).timestamp;
+
+    // Distance calculation between the present information and the last sent in a CPM
+    double delta_distance = calculateDistance(dict->at(obj_id).latitude, dict->at(obj_id).longitude, camera_object.latitude, camera_object.longitude);
+
+    // Speed variation between the present information and the last sent in a CPM
+    double delta_speed = fabs(dict->at(obj_id).speed - camera_object.speed);
+
+    // Heading variation between the present information and the last sent in a CPM
+    double delta_heading = fabs(dict->at(obj_id).speed - camera_object.heading);
+
+    // Calculation of newInfo (according to the CPM rules)
+    if ((delta_timestamp > 1) or (delta_distance > 4) or (delta_speed > 0.5) or (delta_heading > 4)) {
+        return true;
+    }
+
+    return false;
+}
+
+// Calculate the distance between two points given their latitude and longitude
+double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+    // Radius of the Earth in kilometers
+    constexpr double earthRadiusKm = 6371.0;
+
+    // Convert latitude and longitude from degrees to radians
+    lat1 = toRadians(lat1);
+    lon1 = toRadians(lon1);
+    lat2 = toRadians(lat2);
+    lon2 = toRadians(lon2);
+
+    // Haversine formula
+    double dLat = lat2 - lat1;
+    double dLon = lon2 - lon1;
+    double a = sin(dLat / 2) * sin(dLat / 2) +
+               cos(lat1) * cos(lat2) *
+               sin(dLon / 2) * sin(dLon / 2);
+    double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+    double distance = earthRadiusKm * c;
+
+    return distance * 1000;
+}
+
+// Convert degrees to radians
+double toRadians(double degrees) {
+    return degrees * M_PI / 180.0;
+}
+
+std::string prepare_reply(const std::string& request, std::mutex* lock, std::map<int, cameraMqttObject> * objects, std::map<int, cameraMqttObject> * dict_last_sent) {
+    // parse request data
+    rapidjson::Document requestJson;
+    requestJson.Parse(request.c_str());
+
+    unsigned long int requestID = 0;
+    int numberObjects = 0;
+    if (requestJson.HasMember("requestID")){
+        requestID = requestJson["requestID"].GetUint64();
+    }
+    if (requestJson.HasMember("numberObjects")){
+        numberObjects = requestJson["numberObjects"].GetInt();
+    }
+
+//    spdlog::info("RequestID: \"{}\"\n", requestID);
+//    spdlog::info("numberObjects: \"{}\"\n", numberObjects);
+
+    // generate reply json
+    rapidjson::Document replyJson = rapidjson::Document();
+    replyJson.SetObject();
+    rapidjson::Document::AllocatorType& allocator = replyJson.GetAllocator();
+    replyJson.AddMember("requestID", requestID, allocator);
+    replyJson.AddMember("numberObjects", numberObjects, allocator);
+
+    rapidjson::Value objects_json(rapidjson::kArrayType);
+
+    std::lock_guard guard(*lock);
+    for (auto const& [key, value] : *objects) {
+        rapidjson::Value tmpObject(rapidjson::kObjectType);
+        tmpObject.AddMember("heading", value.heading, allocator);
+        tmpObject.AddMember("latitude", value.latitude, allocator);
+        tmpObject.AddMember("longitude", value.longitude, allocator);
+        tmpObject.AddMember("objID", value.objectID, allocator);
+        tmpObject.AddMember("sensorID", 2, allocator);
+        tmpObject.AddMember("speed", value.speed, allocator);
+        tmpObject.AddMember("timestamp", value.timestamp, allocator);
+        tmpObject.AddMember("confidence", value.confidence, allocator);
+
+        // Classification
+        rapidjson::Value classificationArray(rapidjson::kArrayType);
+        rapidjson::Value classificationObject(rapidjson::kObjectType);
+        rapidjson::Value objectClassObject(rapidjson::kObjectType);
+        objectClassObject.AddMember("vehicleSubClass", value.classification, allocator);
+        classificationObject.AddMember("objectClass", objectClassObject, allocator);
+        classificationObject.AddMember("confidence", 101, allocator);   // unavailable (101)
+        classificationArray.PushBack(classificationObject, allocator);
+        tmpObject.AddMember("classification", classificationArray, allocator);
+
+        objects_json.PushBack(tmpObject, allocator);
+
+        // update "last_sent" data
+        dict_last_sent->insert_or_assign(key, value);
+    }
+
+    // add "objects_json" to "replyJson" document
+    replyJson.AddMember("objects", objects_json, allocator);
+
+    return jsonToString(replyJson);
+}
+
+// Convert RapidJSON document to string
+std::string jsonToString(const rapidjson::Document& d) {
+    rapidjson::StringBuffer buffer;
+    rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(buffer);
+    d.Accept(writer);
+
+    return buffer.GetString();
+}
---
+++ camera-adapter/mqtt.cpp
@@ -0,0 +1,133 @@
+#include "mqtt.h"
+
+action_listener::action_listener(std::string name) : name_(std::move(name)) {}
+
+void action_listener::on_failure(const mqtt::token& tok) {
+    if (tok.get_message_id() != 0) {
+        spdlog::warn("{} failure for token: {}", name_, tok.get_message_id());
+    }
+}
+
+void action_listener::on_success(const mqtt::token& tok) {
+    if (tok.get_message_id() != 0) {
+        spdlog::info("{} success for token: {}", name_, tok.get_message_id());
+    }
+
+    auto top = tok.get_topics();
+    if (top && !top->empty()) {
+        spdlog::info("\ttoken topic: {}", (*top)[0]);
+    }
+}
+
+action_listener::~action_listener() {
+}
+
+MqttWrapper::MqttWrapper(mqtt_server data, std::function<void(std::string, std::string)> on_message_received)
+        : client_(data.address, data.client_id), listener_("YourListenerName"), connOpts_(), data_server(std::move(data)){
+
+    this->connOpts_.set_clean_session(false);
+    this->connOpts_.set_automatic_reconnect(true);
+
+    this->client_.set_callback(*this);
+
+    this->on_message_function = on_message_received;
+
+    // Start the connection
+    try {
+        spdlog::info("Connecting to the MQTT server...");
+        this->client_.connect();
+    } catch (const mqtt::exception& exc) {
+        spdlog::error("Unable to connect to MQTT server: ", exc.what());
+        exit(1);
+    }
+    std::thread mqtt_th(&MqttWrapper::from_mqtt_thread, this);
+    mqtt_th.detach();
+}
+
+
+MqttWrapper::~MqttWrapper() {
+}
+
+void MqttWrapper::connect() {
+    try {
+        this->client_.connect(this->connOpts_, nullptr, *this);
+    }
+    catch (const mqtt::exception& exc) {
+        spdlog::error("Error: ", exc.what());
+        exit(1);
+    }
+
+}
+
+void  MqttWrapper::from_mqtt_thread() {
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::seconds(100));
+    }
+}
+
+void MqttWrapper::on_success(const mqtt::token &tok) { }
+
+void MqttWrapper::on_failure(const mqtt::token &tok) {
+    spdlog::warn("MqttWrapper on_failure for token: {}", tok.get_message_id());
+}
+
+void MqttWrapper::connection_lost(const std::string &cause) {
+    spdlog::warn("Connection lost");
+    if (!cause.empty())
+        spdlog::warn("Connection lost caused by {}", cause);
+
+    spdlog::info("Reconnecting...");
+
+    reconnect();
+}
+
+void MqttWrapper::connected(const std::string &cause) {
+    spdlog::info("Connection success");
+    this->subscribe();
+}
+
+bool MqttWrapper::is_connected() {
+    return this->client_.is_connected();
+}
+
+void MqttWrapper::reconnect() {
+    std::this_thread::sleep_for(std::chrono::milliseconds(2500));
+    this->connect();
+}
+
+void MqttWrapper::subscribe() {
+    this->client_.subscribe(data_server.subscription_topic, data_server.qos, nullptr, this->listener_);
+}
+
+
+void MqttWrapper::message_arrived(mqtt::const_message_ptr msg) {
+    on_message_function(msg->get_topic(), msg->to_string());
+
+}
+
+void MqttWrapper::delivery_complete(mqtt::delivery_token_ptr token) {
+    spdlog::info("Delivery complete");
+
+}
+
+void MqttWrapper::publish(const std::string &topic, const std::string &message) {
+    try {
+        this->client_.publish(topic, message);
+    }
+    catch (const mqtt::exception& exc) {
+        spdlog::error("Error: ", exc.what());
+    }
+}
+
+void MqttWrapper::disconnect(){
+    try {
+        spdlog::info("Disconnecting from the MQTT server...");
+        this->client_.disconnect()->wait();
+        spdlog::info("Disconnected");
+    }
+    catch (const mqtt::exception& exc) {
+        spdlog::error("Error: ", exc.what());
+        exit(1);
+    }
+
+}
